9597d24e0fa2ad71af8d3d201f44063fe00e62b6
thieuphanchinh <chinhtp62@wru.vn>
2019-01-01 09:30:14 +0700

    Ý tưởng thêm tính năng nho nhỏ cho web :))

--- 2. Sources/dash.php
+++ 2. Sources/dash.php
@@ -210,13 +210,13 @@
         // }
         // echo '</table></div>';}
         // }
-
+//phần hiển thị xếp hạng trong admin
         if(@$_GET['q']== 2) 
 {
 $q=mysqli_query($con,"SELECT * FROM rank  ORDER BY score DESC " )or die('Error223');
 echo  '<div class="panel title">
 <table class="table table-striped title1" >
-<tr style="color:red"><td><b>Rank</b></td><td><b>Name</b></td><td><b>Gender</b></td><td><b>College</b></td><td><b>Score</b></td></tr>';
+<tr style="color:red"><td><b>Hạng</b></td><td><b>Tên</b></td><td><b>Giới tính</b></td><td><b>Lớp</b></td><td><b>Điểm</b></td></tr>';
 $c=0;
 while($row=mysqli_fetch_array($q) )
 {
